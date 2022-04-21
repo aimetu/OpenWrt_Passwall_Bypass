d26196e6c057d687858d12a9eee4878ed5df6c19
aimetu <41498149+aimetu@users.noreply.github.com>
2022-04-21 11:47:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_generate
 
 # 取消bootstrap为默认主题
-#ed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
