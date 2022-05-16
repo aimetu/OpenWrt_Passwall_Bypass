b5835c58e145fb1e552631e71155b4e0a5f14c9a
aimetu <41498149+aimetu@users.noreply.github.com>
2022-05-16 12:06:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,17 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# Modify default IP 默认网关IP修改
 sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_generate
 
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-# Modify default theme
+# Modify default theme设置默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
 
-# Add OpenAppFilter
+# Add OpenAppFilter 应用过滤插件
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# 更换5.4内核为5.10
+#sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
